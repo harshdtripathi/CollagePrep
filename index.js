f5ae14f9777032ae261be82745f674a01f43bec0
Harsh Deepak Tripathi <128794097+harshdtripathi@users.noreply.github.com>
2025-06-20 11:47:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const authroute= require("./Routes/Profile")
 
 // Load environment variables
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
 connectDB();
@@ -47,8 +47,9 @@ app.use("/api/auth", authroute);
 // app.use("/api/papers", require("./routes/paperRoutes"));
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+module.exports=app;
 
