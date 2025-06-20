2bb92dc3ce83e1012aae44b9a0a65c762718b40f
Harsh Deepak Tripathi <128794097+harshdtripathi@users.noreply.github.com>
2025-06-20 14:02:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,29 +15,29 @@ connectDB();
 const app = express();
 
 // Middleware
-// const allowedOrigins = [
-//   "http://localhost:5173",
+const allowedOrigins = [
+  "http://localhost:5173",
   
-// ];
+];
 
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       if (!origin || allowedOrigins.includes(origin)) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     },
-//     credentials: true,
-//   })
-// );
 app.use(
   cors({
-    origin: true, // Reflects the request origin
-    credentials: true, // Allow cookies (Authorization headers, etc.)
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
   })
 );
+// app.use(
+//   cors({
+//     origin: true, // Reflects the request origin
+//     credentials: true, // Allow cookies (Authorization headers, etc.)
+//   })
+// );
 
 app.use(express.json()); // Accept JSON data
 app.use(cookieParser());
